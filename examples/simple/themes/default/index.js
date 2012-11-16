afbcb24d4ff5b2d130745bbb500330dd17458ac2
Pablo Ricco <pricco@gmail.com>
2012-11-16 18:34:52 -0200

    default style

--- examples/simple/themes/default/index.js
+++ examples/simple/themes/default/index.js
@@ -1,11 +1,16 @@
 
 var Theme = function(site) {
 	this._site = site;
+	site.on('prerender', this.preRender.bind(this));
+};
 
-	site.on('prerender', function(request, response, doc){
-		response.locals.title = doc.getMeta()['title'] || 'Simple';
-	});
-	
+Theme.prototype.preRender = function(request, response, doc) {
+	var settings = this._site.getSettings();
+	response.locals.site = response.locals.site || {};
+	response.locals.site.title = settings.get('title') || 'Default';
+	response.locals.site.menus = settings.get('menus') || {};
+	response.locals.title = doc.getMeta()['title'] || 'Document';
+
 };
 
 module.exports = Theme;
